6f8cdc5ea3ed4124258b2eb0e99b9aff202db74f
neyrzx <khorev.valeriy@gmail.com>
2025-06-06 15:29:53 +1200

    minimal working version

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -7,7 +7,7 @@ OUT_DIR="${PWD}/pkg"
 rm -rf "${OUT_DIR}"
 mkdir -p "${OUT_DIR}"
 
-docker run --rm --user "$(id -u)" --volume "${PWD}":"${PWD}" --workdir "${PWD}" tool-protoc:latest \
+docker run --rm --user "$(id -u)" --volume "${PWD}":"${PWD}" --workdir "${PWD}" ryme/tool-protoc:latest \
   --proto_path "${PWD}" \
   --go_out "${OUT_DIR}" \
   --go_opt paths=source_relative \
